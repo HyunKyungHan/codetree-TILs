626828b8ee27a50e4deeea22b3f98330962d90b0
Hyun Kyung Han <hhk@ewhain.net>
2024-05-31 18:14:24 +0900

    Title: 스승의 은혜, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240531/스승의 은혜/the-grace-form-teacher.py
@@ -0,0 +1,27 @@
+def count_affordable(mat, budget):
+    cost = 0
+    st_count = 0
+    for i in range(len(mat)):
+        cost += sum(mat[i])
+        if budget >= cost:
+            st_count += 1
+    return st_count
+
+def discount(mat, budget):
+    max_std = 0
+    cost = 0
+    for i in range(len(mat)):
+        cost += sum(mat[i])
+    for i in range(len(mat)):
+        mat[i][0] = mat[i][0]//2
+        if count_affordable(mat, budget) > max_std:
+            max_std = count_affordable(mat, budget)
+    return max_std
+
+if __name__ == '__main__':
+    s_num, budget  = map(int, input().split())
+    costs = []
+    for i in range(s_num):
+        costs.append(list(map(int, input().split())))
+    costs.sort()
+    print(discount(costs, budget))
